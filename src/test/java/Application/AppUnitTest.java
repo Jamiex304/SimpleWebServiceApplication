a5997a61e3da25e79b232b8bd76de25a1d77b7e6
jamie.mulvaney <jamie.mulvaney@bearingpoint.com>
2020-08-13 13:20:44 +0100

    Simple Maven App With 71% Code Coverage

---
+++ src/test/java/Application/AppUnitTest.java
@@ -0,0 +1,32 @@
+package Application;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppUnitTest {
+
+    @Test
+    public void whenEmptyString_thanAccept() {
+        App app = new App();
+        assertTrue(app.isPalindrome(""));
+    }
+
+    @Test
+    public void whenPalindrom_thanAccept() {
+        App app = new App();
+        assertTrue(app.isPalindrome("noon"));
+    }
+
+//    @Test
+//    public void whenNotPalindrom_thanReject(){
+//        App app = new App();
+//        assertFalse(app.isPalindrome("box"));
+//    }
+
+//    @Test
+//    public void whenNearPalindrom_thanReject(){
+//        App app = new App();
+//        assertFalse(app.isPalindrome("neon"));
+//    }
+}
